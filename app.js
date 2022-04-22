02201df10e7f4e3dd90215d3a113defcccddefd6
Jawid <41361157+infjay@users.noreply.github.com>
2022-04-22 11:57:41 +0200

    Route params-creating a generic route for all products

--- app.js
+++ app.js
@@ -1,76 +1,43 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/Product.model");
 const app = express();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-
+/* Connect to DB */
+mongoose
+  .connect("mongodb://localhost/ironborn-ecommerce")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 /* Routes */
 
-app.get("/", (req, res, next)=>{
-    res.render("home");
+app.get("/", (req, res, next) => {
+  res.render("home");
 });
-
 
 app.get("/about", (req, res, next) => {
-    res.render("about");
+  res.render("about");
 });
-
 
 app.get("/contact", (req, res, next) => {
-    res.render("contact");
+  res.render("contact");
 });
 
-
-
-
-app.get("/limoncello", (req, res, next) => {
-
-    const data = {
-        title: "Limoncello",
-        price: 20,
-        imageFile: "product-limoncello.png",
-        stores: ["Online", "Albacete", "Freiburg", "Amsterdam"]
-    }
-
-    res.render("product", data);
+app.get("/products/:productId", (req, res, next) => {
+  Product.findById(req.params.productId)
+    .then((productDetails) => {
+      res.render("product", productDetails);
+    })
+    .catch((error) => console.log("error getting product from DB", error));
 });
-
-
-app.get("/whisky", (req, res, next) => {
-
-    const data = {
-        title: "Single Malt Whisky Yamakazi",
-        price: 105,
-        imageFile: "whisky-yamazaki.jpg"
-    }
-
-    res.render("product", data);
-});
-
-
-app.get("/tequila", (req, res, next) => {
-
-    const data = {
-        title: "Tequila Don Julio",
-        price: 35,
-    }
-
-    res.render("product", data);
-});
-
-
-
-
-
-
 
 app.listen(3001, () => {
-    console.log("server listening to requests...")
+  console.log("server listening to requests...");
 });
-
-
